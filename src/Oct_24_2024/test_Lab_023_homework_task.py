7ae2e58635e678524b7d8113a620626904e62916
bhargavi539 <48649370+bhargavi539@users.noreply.github.com>
2024-10-25 20:31:25 +0200

    homework on selecting radio, checkbox

---
+++ src/Oct_24_2024/test_Lab_023_homework_task.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import time
+import allure
+
+@allure.title("Selecting the options from the dropdown or select box")
+def test_homework_task():
+    driver =  webdriver.Chrome()
+    driver.get("https://awesomeqa.com/practice.html")
+
+    gender = driver.find_element(By.CSS_SELECTOR,"input#sex-1")
+    gender.click()
+
+    profession_checkbox = driver.find_element(By.XPATH,"//input[@id='profession-1']")
+    profession_checkbox.click()
+
+    year_of_exp = driver.find_element(By.ID,"exp-2")
+    year_of_exp.click()
+
+    time.sleep(3)
